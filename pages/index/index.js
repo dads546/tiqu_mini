adc148ef6df2bd716d9cd4410548d731a06ecd22
songxilong <641798770@qq.com>
2024-09-12 23:17:37 +0800

    轮播图
    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,25 @@
 const swiperList = [
-  '4.jpeg',
-  '5.jpeg'
+  './4.jpeg',
+  './5.jpeg'
 ];
 
 Page({
   data: {
-    indicatorDots: true,
-    autoplay: true,
-    interval: 3000,
-    duration: 1000,
-    imageUrls: ['4.jpeg',
-    '5.jpeg'],
+    images: [
+      './4.jpeg',
+      './5.jpeg'
+    ]
   },
+  
+  onSwiperChange(event) {
+    console.log('Current swiper index:', event.detail.current);
+  },
+
+  onImageTap(event) {
+    // const { current } = event.detail;
+    // wx.previewImage({
+    //   current: this.data.images[current], // 当前显示的图片
+    //   urls: this.data.images // 需要预览的图片链接列表
+    // });
+  }
 })

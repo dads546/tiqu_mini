aba48e28cdc4c1922b5fff082f6f787623b2defc
songxilong <songxilong@163.com>
2024-09-12 20:43:48 +0800

    测试
    测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,15 @@
+const swiperList = [
+  '4.jpeg',
+  '5.jpeg'
+];
+
 Page({
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    imageUrls: ['4.jpeg',
+    '5.jpeg'],
+  },
 })

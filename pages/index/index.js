fa05a4a60bd6dabdaab0b5cfa04c7a3c84fb06fd
songxilong <songxilong@163.com>
2024-09-25 14:26:39 +0800

    feat: 增加首页分享

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,11 @@
 Page({
+  onShareAppMessage(){//点亮发送给朋友
+    return {
+      title: '一键提取王：素材小助手',//标题
+      path: 'pages/index/index'//路径
+    }
+  },
+  onShareTimeline() {},
   navigateToDytj() {
     wx.navigateTo({
       url: '/pages/dytj/index',
